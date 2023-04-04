8c9ac34c0ac084740321547857a898fe89cc20f6
Muhammedshifin <muhammedshifink4@gmail.com>
2023-04-04 22:29:43 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -70,22 +70,6 @@ async def approve(client: User, message: Message):
     await asyncio.sleep(5)
     await msg.delete()
  
-@User.on_message(filters.command(["no", "remove", "decline"], C) & authchat)                     
-async def decline(client: User, message: Message):
-    chat=message.chat 
-    try:
-       try:
-          await client.decline_all_chat_join_requests(chat.id)
-          return   
-       except FloodWait as t:
-          asyncio.sleep(t.value)
-          await client.decline_all_chat_join_requests(chat.id)
-          return     
-    except Exception as e:
-       print(e)
-    hhh = await client.send_message(chat.id, "mission completed ❌️ declined all joinrequest")  
-    await asyncio.sleep(3)
-    await hhh.delete()     
 
 print("bot started....")
 User.run()
